f27dfbab3b2c2408dcae1266bd90055b2923e86c
Manuel Maier <mjmaier@gmx.de>
2016-10-02 14:56:24 +0200

    Script to generate sublime-project file

---
+++ tools/generate_sublime_project.py
@@ -0,0 +1,50 @@
+from pathlib import *
+import argparse
+import json
+
+thisDir = Path(__file__).parent.resolve()
+repoDir = thisDir.parent
+
+def main(destFilePath):
+  content = {}
+  content["folders"] = [{ 'path': str(repoDir) }]
+  content["build_systems"] = [
+    {
+      "name": "mtb",
+      "file_regex": "([A-z]:.*?)\\(([0-9]+)(?:,\\s*[0-9]+)?\\)",
+
+      "variants":
+      [
+        {
+          "name": "Tests: Build Only",
+          "windows":
+          {
+            "cmd": [ "{}/tests/build.bat".format(repoDir) ],
+          },
+        },
+        {
+          "name": "Tests: Build and Run",
+          "windows":
+          {
+            "cmd": [ "{}/tests/build.bat".format(repoDir) ],
+          },
+        }
+      ]
+    },
+  ]
+
+  if not destFilePath.parent.exists():
+    destFilePath.parent.mkdir(parents=True)
+
+  with destFilePath.open('w') as destFile:
+    json.dump(content, destFile)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('dest',
+                      nargs='?',
+                      type=Path,
+                      default=(repoDir / 'workspace' / 'mtb.sublime-project'),
+                      help='The destination file.')
+  args = parser.parse_args()
+  main(destFilePath=args.dest)
