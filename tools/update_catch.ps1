8d66bf55520d957177974e88949edcadb0e8bfac
Manuel Maier <mjmaier@gmx.de>
2017-11-05 15:51:59 +0100

    Updated catch.

--- tools/update_catch.ps1
+++ tools/update_catch.ps1
@@ -1,18 +1,19 @@
+$Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
 $RepoRoot = Get-Item (Join-Path $PSScriptRoot "..")
-$CatchReleasesURL = "https://api.github.com/repos/philsquared/Catch/releases"
+$CatchReleasesURL = "https://api.github.com/repos/catchorg/Catch2/releases"
 $CatchDestinationFilePath = Join-Path $RepoRoot "tests/catch.hpp"
 
 # Find the latest release
 $AllReleases = (Invoke-WebRequest $CatchReleasesURL -UseBasicParsing).Content | ConvertFrom-Json
 $LatestRelease = $AllReleases[0]
 $LatestAssets = $LatestRelease.assets
-$LatestCatchHeaderAssets = $AllReleases[0].assets | where { $_.name -eq "catch.hpp" }
+$LatestCatchHeaderAssets = $LatestAssets | where { $_.name -eq "catch.hpp" }
 $HeaderDownloadURL = $LatestCatchHeaderAssets[0].browser_download_url
 
 # Download the file.
 Write-Host "Downloading: $HeaderDownloadURL => $CatchDestinationFilePath"
 Write-Progress "Updating Catch" "Downloading: $HeaderDownloadURL => $CatchDestinationFilePath"
-$DownloadTime = Measure-Command {
-  (New-Object System.Net.WebClient).DownloadFile($HeaderDownloadURL, $CatchDestinationFilePath)
-}
-Write-Host ("Downloaded in {0:N2}s." -f $DownloadTime.TotalSeconds)
+(New-Object System.Net.WebClient).DownloadFile($HeaderDownloadURL, $CatchDestinationFilePath)
+
+Write-Host ("Updated in {0:N2}s." -f $Stopwatch.Elapsed.TotalSeconds)
